7a57e2f6bf6a6b15baf54225dee60a9e6514b38b
yinqiao <1737666820@qq.com>
2023-01-18 09:29:48 +0800

    feat: 完成

--- src/utils/fetchOperations.ts
+++ src/utils/fetchOperations.ts
@@ -1,11 +1,18 @@
+import * as vscode from "vscode"
 import { UrlLoader } from "@graphql-tools/url-loader"
 import { loadSchema } from "@graphql-tools/load"
 import { getOperationsBySchema } from "@fruits-chain/qiufen-helpers"
+import { GraphQLSchema } from "graphql"
 
 export default async function fetchOperations(endpointUrl: string) {
-  const schema = await loadSchema(endpointUrl, {
-    loaders: [new UrlLoader()],
-  })
-  const operations = getOperationsBySchema(schema)
+  let schema: GraphQLSchema
+  try {
+    schema = await loadSchema(endpointUrl, {
+      loaders: [new UrlLoader()],
+    })
+  } catch (error: any) {
+    vscode.window.showErrorMessage("gql-doc schema地址配置错误或者网络出错啦！！！")
+  }
+  const operations = getOperationsBySchema(schema!)
   return operations
 }
