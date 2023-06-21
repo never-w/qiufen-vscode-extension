ebe639182e1c03c59a87deb73c5a07435506116c
yinqiao <1737666820@qq.com>
2023-06-21 15:33:46 +0800

    sa

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
-const { DefinePlugin } = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
@@ -29,60 +27,13 @@ const extensionConfig = {
     },
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'],
   },
-  plugins: [
-    // 解决react开发的嵌套webview缺失node环境下的process.env
-    // new DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify((process.env.NODE_ENV = 'production')),
-    //   },
-    // }),
-    new CleanWebpackPlugin(),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: "./src/server-mock/public",
-    //       to: "./public",
-    //     },
-    //   ],
-    // }),
-  ],
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: '/node_modules/',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.module\.less$/,
-        use: [
-          // compiles Less to CSS
-          'style-loader',
-          'css-loader',
-          'less-loader',
-          {
-            loader: 'style-resources-loader',
-            options: {
-              patterns: ['./src/webview/styles/*.less'],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/, // 针对这三种格式的文件使用file-loader处理
-        use: {
-          loader: 'file-loader',
-          options: {
-            // 定义打包后文件的名称；
-            // [name]:原文件名，[hash]:hash字符串（如果不定义名称，默认就以hash命名，[ext]:原文件的后缀名）
-            name: '[name].[ext]',
-            outputPath: 'images/', //  定义图片输出的文件夹名（在output.path目录下）
-          },
-        },
       },
     ],
   },
